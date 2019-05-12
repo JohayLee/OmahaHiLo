fbb859191edf3e9469934470c2ee6bfe84e2e698
JohayLee <50304672+JohayLee@users.noreply.github.com>
2019-05-12 17:32:25 -0400

    Clean-up some debugging intermediate code.

--- src/main/java/OmahaHiLo/JavaPractice/RankingRules/Straight.java
+++ src/main/java/OmahaHiLo/JavaPractice/RankingRules/Straight.java
@@ -38,31 +38,6 @@ public class Straight extends RankingHigh {
 	}
 	public static boolean CheckStraight(Card[] aHandOfCards, boolean isAceTopRanking)
 	{
-		// Sort the cards based on ranks
-		boolean is23456 = false;
-		boolean is65432 = false;
-		if (aHandOfCards[0].GetRankValue(isAceTopRanking) == 2 
-			&& aHandOfCards[1].GetRankValue(isAceTopRanking) == 3
-			&& aHandOfCards[2].GetRankValue(isAceTopRanking) == 4
-			&& aHandOfCards[3].GetRankValue(isAceTopRanking) == 5
-			&& aHandOfCards[4].GetRankValue(isAceTopRanking) == 6)
-		{
-			is23456 = true;
-		}
-		aHandOfCards = PokerUtils.SortCardsDescending(aHandOfCards, isAceTopRanking);
-		if (aHandOfCards[0].GetRankValue(isAceTopRanking) == 6 
-			&& aHandOfCards[1].GetRankValue(isAceTopRanking) == 5
-			&& aHandOfCards[2].GetRankValue(isAceTopRanking) == 4
-			&& aHandOfCards[3].GetRankValue(isAceTopRanking) == 3
-			&& aHandOfCards[4].GetRankValue(isAceTopRanking) == 2)
-		{
-			is65432 = true;
-		}
-		boolean isAny = false;
-		if (is23456 || is65432)
-		{
-			isAny = true;
-		}
 		// The delta between two elements shall be 1.
 		for (int i = 0; i < aHandOfCards.length - 1; i++)
 		{
@@ -77,31 +52,6 @@ public class Straight extends RankingHigh {
 				return false;
 			}
 		}
-		is23456 = false;
-		is65432 = false;
-		if (aHandOfCards[0].GetRankValue(isAceTopRanking) == 2 
-			&& aHandOfCards[1].GetRankValue(isAceTopRanking) == 3
-			&& aHandOfCards[2].GetRankValue(isAceTopRanking) == 4
-			&& aHandOfCards[3].GetRankValue(isAceTopRanking) == 5
-			&& aHandOfCards[4].GetRankValue(isAceTopRanking) == 6)
-		{
-			is23456 = true;
-		}
-		aHandOfCards = PokerUtils.SortCardsDescending(aHandOfCards, isAceTopRanking);
-		if (aHandOfCards[0].GetRankValue(isAceTopRanking) == 6 
-			&& aHandOfCards[1].GetRankValue(isAceTopRanking) == 5
-			&& aHandOfCards[2].GetRankValue(isAceTopRanking) == 4
-			&& aHandOfCards[3].GetRankValue(isAceTopRanking) == 3
-			&& aHandOfCards[4].GetRankValue(isAceTopRanking) == 2)
-		{
-			is65432 = true;
-		}
-		isAny = false;
-		if (is23456 || is65432)
-		{
-			isAny = true;
-		}
-		
 		return true;
 		
 	}
